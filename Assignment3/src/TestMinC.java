4d26da30ded9d0e27c231037f218b3b6c971680c
sh92 <sanghee.lee1992@gmail.com>
2016-11-07 22:45:19 +0900

    Compiler Antlr

---
+++ Assignment3/src/TestMinC.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+
+public class TestMinC {
+	public static void main(String[] args) throws IOException{
+		MiniCLexer lexer = new MiniCLexer(new ANTLRFileStream("test.c"));
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		MiniCParser parser =new MiniCParser(tokens);
+		ParseTree tree = parser.program();
+
+		ParseTreeWalker walker = new ParseTreeWalker();
+		walker.walk(new MiniCPrintListener(), tree);
+	}
+}
---
+++ Assignment3/test.c
@@ -0,0 +1,17 @@
+int max=500; void main(){ int i; int j; int k; int rem; int sum;
+  i = 2;
+  while(i <= max) {
+         sum = 0;
+         k = i / 2;
+         j = i;
+         while ( j <= k) {
+            rem = i % j;
+            if (rem == 0) {
+            sum=sum+j;
+            ++j;
+         }
+        }
+    if (i == sum) write(i);
+    ++i;
+    }
+  }
